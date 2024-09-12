ca7e3187ae046947a8358e676b9750893f1280b8
Vinicius Martins <vinicius.martins@tqi.com.br>
2024-09-12 16:55:10 -0300

    first commit

---
+++ src/main/java/viniciusmartins/github/mongoapp/MongoappApplication.java
@@ -0,0 +1,13 @@
+package viniciusmartins.github.mongoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoappApplication.class, args);
+	}
+
+}
